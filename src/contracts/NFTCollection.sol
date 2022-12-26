f8cac47e42ffcf917fab3c3fadbc46939c6336bf
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2022-12-26 01:56:25 -0500

    Update NFTCollection.sol

--- src/contracts/NFTCollection.sol
+++ src/contracts/NFTCollection.sol
@@ -14,7 +14,7 @@ contract NFTCollection is ERC721, ERC721Enumerable {
   {
   }
 
-  function _beforeTokenTransfer(address from, address to, uint256 tokenId)  {
+  function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal override(ERC721, ERC721Enumerable) {
     super._beforeTokenTransfer(from, to, tokenId);
   }
 
