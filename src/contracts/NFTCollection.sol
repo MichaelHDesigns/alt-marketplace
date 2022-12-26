73f5a601e8bb7d2686ae535ce90875c4c64ac837
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2022-12-26 01:58:09 -0500

    Update NFTCollection.sol

--- src/contracts/NFTCollection.sol
+++ src/contracts/NFTCollection.sol
@@ -1,38 +1,42 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+contract MinterYT is ERC721, ERC721Enumerable, ERC721URIStorage {
+    using SafeMath for uint256;
+    uint public constant mintPrice = 0;
 
-contract NFTCollection is ERC721, ERC721Enumerable {
-  string[] public tokenURIs;
-  mapping(string => bool) _tokenURIExists;
-  mapping(uint => string) _tokenIdToTokenURI;
-
-  constructor() 
-    ERC721("mTC Collection", "mTC") 
-  {
-  }
-
-  function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal override(ERC721, ERC721Enumerable) {
-    super._beforeTokenTransfer(from, to, tokenId);
-  }
-
-  function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721Enumerable) returns (bool) {
-    return super.supportsInterface(interfaceId);
-  }
-
-  function tokenURI(uint256 tokenId) public override view returns (string memory) {
-    require(_exists(tokenId), 'ERC721Metadata: URI query for nonexistent token');
-    return _tokenIdToTokenURI[tokenId];
-  }
-
-  function safeMint(string memory _tokenURI) public {
-    require(!_tokenURIExists[_tokenURI], 'The token URI should be unique');
-    tokenURIs.push(_tokenURI);    
-    uint _id = tokenURIs.length;
-    _tokenIdToTokenURI[_id] = _tokenURI;
-    _safeMint(msg.sender, _id);
-    _tokenURIExists[_tokenURI] = true;
-  }
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+    constructor() ERC721("ALT Devil Token", "ADT") {}
+    function mint(string memory _uri) public payable {
+        uint256 mintIndex = totalSupply();
+        _safeMint(msg.sender, mintIndex);
+        _setTokenURI(mintIndex, _uri);
+    }
 }
